0b202fbe3bc715a4f07e6bd814c47cbcd0d5beb7
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-05 23:03:06 +0530

    add initial

---
+++ src/main/java/com/nest/libraryapp_backend/LibraryappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.libraryapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/libraryapp_backend/LibraryappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.libraryapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
